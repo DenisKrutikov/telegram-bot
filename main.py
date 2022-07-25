682be49e6ac5d992be9e028695319a815bdf6aae
Denis Krutikov <deniskrutikov1989@gmail.com>
2022-07-25 20:14:53 +0700

    01-creating_bot

---
+++ main.py
@@ -0,0 +1,17 @@
+import telebot
+
+
+bot = telebot.TeleBot('5447245522:AAEMPEvunSeaQVzz5qkIkZaZN4FvtwjSxzY')
+
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == 'Привет':
+        bot.send_message(message.from_user.id, 'Привет, чем я могу тебе помочь?')
+    elif message.text == '/hello_world':
+        bot.send_message(message.from_user.id, 'Напиши привет')
+    else:
+        bot.send_message(message.from_user.id, 'Я тебя не понимаю.')
+
+
+bot.polling(none_stop=True)
