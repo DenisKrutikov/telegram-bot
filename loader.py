9a839d1c705c4a85c448c19e742b68bebaa43c75
Denis Krutikov <deniskrutikov1989@gmail.com>
2023-02-10 18:55:55 +0700

    04-fixed error, added type annotation

--- loader.py
+++ loader.py
@@ -2,15 +2,21 @@ import logging
 import telebot
 import sqlite3
 from config_data import config
+from telebot_calendar import Calendar, RUSSIAN_LANGUAGE, CallbackData
 
 
 bot = telebot.TeleBot(config.BOT_TOKEN, parse_mode='HTML')
 logger = telebot.logging
 telebot.logging.basicConfig(
-    filename='filename.log',
-    level=logging.DEBUG,
+    filename='log/logfile.log',
+    level=logging.INFO,
     format=' [%(asctime)s - %(levelname)s] - %(message)s',
     encoding='UTF-8'
+)
+
+calendar = Calendar(language=RUSSIAN_LANGUAGE)
+calendar_1_callback = CallbackData(
+    "calendar_1", "action", "year", "month", "day"
 )
 
 with sqlite3.connect('database/search_history.db') as sqlite_connection:
--- main.py
+++ main.py
@@ -2,18 +2,25 @@ import datetime
 import telebot
 from telebot import types
 from config_data.config import DEFAULT_COMMANDS
-from loader import bot
+from loader import bot, calendar, calendar_1_callback
 from users.user_info import Users
+from telebot.types import Message, CallbackQuery
 from handlers.hotel_price import get_photo_hotels, min_price, get_number_hotels
 from handlers.hotel_price import start_search, result
 from handlers.history import history_command
-from utils.bot_methods import calendar, calendar_1_callback, add_calendar
+from utils.bot_methods import add_calendar
 
 
 @bot.message_handler(commands=['start'])
-def start_command(message):
+def start_command(message: Message) -> None:
+    """
+    Функция, которая запускает бота
+    и начинает диалог с пользователем.
+
+    :param message: передается сообщение чата телеграмм-бота.
+    """
     bot.send_message(
-        message.from_user.id,
+        chat_id=message.from_user.id,
         text='Здравствуйте, {name}.\n'
              'Я помогу тебе подобрать отель.\n'
              'Для начала работы набери /help.'.
@@ -22,9 +29,15 @@ def start_command(message):
 
 
 @bot.message_handler(
-    commands=['lowprice', 'highprice', 'beastdeal', 'history']
+    commands=['lowprice', 'beastdeal', 'history']
 )
-def price_command(message):
+def price_command(message: Message) -> None:
+    """
+    Функция, обрабатывает команды и начинает
+    поиск отелей или выводит историю поиска.
+
+    :param message: передается сообщение чата телеграмм-бота.
+    """
     user = Users.get_user(message.from_user.id)
     user.cleaning()
     user.command = message.text
@@ -37,7 +50,13 @@ def price_command(message):
 @bot.callback_query_handler(
     func=lambda call: call.data.startswith(calendar_1_callback.prefix)
 )
-def callback_inline(call):
+def callback_inline(call: CallbackQuery) -> None:
+    """
+    Функция обрабатывает нажатие кнопки в календаре,
+    записывает полученную дату и регистрирует следующий шаг поиска.
+
+    :param call:
+    """
     name, action, year, month, day = call.data.split(calendar_1_callback.sep)
     date = calendar.calendar_query_handler(
         bot=bot,
@@ -105,7 +124,13 @@ def callback_inline(call):
 
 
 @bot.callback_query_handler(func=lambda call: True)
-def callback_worker(call):
+def callback_worker(call: CallbackQuery) -> None:
+    """
+    Функция обрабатывает нажатие на кнопку выбора фото отелей
+    и регистрирует следующий шаг поиска.
+
+    :param call:
+    """
     if call.data == 'yes':
         msg = bot.send_message(
             chat_id=call.message.chat.id,
@@ -119,21 +144,31 @@ def callback_worker(call):
 
 
 @bot.message_handler(commands=['help'])
-def bot_help(message):
+def bot_help(message: Message) -> None:
+    """
+    Функция обрабатывает команду 'help' и выводит помощь в чат.
+
+    :param message: передается сообщение чата телеграмм-бота.
+    """
     text = [f'/{command} - {desk}' for command, desk in DEFAULT_COMMANDS]
     bot.reply_to(message, '\n'.join(text))
 
 
 @bot.message_handler(content_types=['text'])
-def get_text_messages(message):
+def get_text_messages(message: Message) -> None:
+    """
+    Функция обрабатывает приветствие и неизвестные для бота команды.
+
+    :param message: передается сообщение чата телеграмм-бота.
+    """
     if message.text.lower() == 'привет':
         bot.send_message(
-            message.from_user.id,
+            chat_id=message.from_user.id,
             text='Привет, чем я могу тебе помочь?'
         )
     else:
         bot.send_message(
-            message.from_user.id,
+            chat_id=message.from_user.id,
             text='Я тебя не понимаю. Введите /help. '
         )
 
